b784113b6cb96247de8543557c5458ed47f7048e
William Panting <willtp87@hotmail.com>
2012-02-14 14:37:45 -0400

    first commit of demo

---
+++ islandora_training_demo_hello_islandora.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file
+ *   This is a demo template file for the hello_islandora page.
+ *
+ * @author William Panting
+ */
+  print('This message is from the theming layer.  The realm of usability, accessability, and candy.  We use templates to make our code accessable to themers.');
+?>
+<p>A template is essentialy an html/php page that has had information made available to it by the drupal module.</p>
